a08a644eae6f933a7f77db7649939f8be04cf55d
webitproff <webitproff@gmail.com>
2026-08-10 15:08:52 +0300

    Update Russian language file for xtradbrowpage plugin
    Updated plugin version and date in Russian language file. Added plugin information and notes.

--- xtradbrowpage/lang/xtradbrowpage.ru.lang.php
+++ xtradbrowpage/lang/xtradbrowpage.ru.lang.php
@@ -2,17 +2,51 @@
 /**
  * Russian Language File for xtradbrowpage Plugin
  *
- * Date: Apr 25Th, 2026
+ * Date: Aug 10Th, 2026
  * @package xtradbrowpage
- * @version 2.7.8
+ * @version 3.0.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowpage-cotonti
  * @license BSD
  */
 
 defined('COT_CODE') or die('Wrong URL.');
+// использовать глобальную переменную $db_x, которая определена в datas/config.php 
+// и доступна абсолютно всегда, ещё до загрузки любых плагинов 
+// $db_x — это не устаревшая глобальная переменная, а малоизвестная, 
+// ключевая переменная для например таких задач для корректно ссылки.
+// задаётся в конфиге datas/config.php и пробрасывается через 
+// Cot::init() в system/common.php используя class Cot из Cot.php . 
+// Она работает и до установки плагина, и после.
+// В Cotonti нет других надёжных способов получить префикс таблиц на этапе загрузки языкового файла. 
+// Cot::$db_x и Cot::$db->tablePrefix не являются частью публичного API и не гарантируют доступность в нужный момент. 
+// Переменная $db_x, определённая в datas/config.php и доступная через global, — это единственный корректный и документированный способ. 
+// Поэтому выражение с $db_x является правильным и единственно верным для данной ситуации.
 
+global $db_x;
+
+$main_url = rtrim(Cot::$cfg['mainurl'], '/');
+$url = $main_url . '/' . cot_url('admin', 'm=extrafields&n=' . $db_x . 'xtradbrowpage', '', true);
+
+$L['xtradbrowpage'] = 'Extrafields Pages Custom'; // устанавливаем ключ и определяем перевенную до вызова в ссылке!
 // Plugin configuration
 
 
-$L['xtradbrowpage'] = 'Кастомные экстраполя страниц';
+/**
+ * Plugin Info
+ */
+$L['info_name'] = 'Extrafields Pages Custom';
+
+$L['info_desc'] = 'Плагин добавляет экстраполя для модуля <code>page</code> в свою собственную таблицу БД. Файл README.md читать прежде всего!';
+
+$L['info_notes'] = 
+    'Файл <a href="https://github.com/webitproff/xtradbrowpage-cotonti/blob/main/README.md" target="_blank"><strong>README.md</strong></a> читать прежде всего! <br>' .
+    'Новичкам ' .
+    '<a href="https://abuyfile.com/ru/forums/cotonti/original/extrafields" target="_blank">' .
+    '<abbr title="Введение. Описание и принципы работы экстраполей в Cotonti" class="initialism">' .
+    '<strong>обязательно читать раздел форума об API ExtraFields</strong></abbr></a>. <br>' . 
+    'После установки плагина, открыть экстраполя плагина ' .
+    '<a href="' . $url . '" target="_blank">' .
+    '<strong> ' . $L['xtradbrowpage'] . ' </strong></a>.';
+
+
