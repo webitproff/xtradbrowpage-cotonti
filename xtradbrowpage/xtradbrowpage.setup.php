8ff1c25436eee4eda2a9e6cca7a550360c866219
webitproff <webitproff@gmail.com>
2026-08-10 15:06:54 +0300

    Update xtradbrowpage.setup.php

--- xtradbrowpage/xtradbrowpage.setup.php
+++ xtradbrowpage/xtradbrowpage.setup.php
@@ -4,16 +4,17 @@
 Code=xtradbrowpage
 Name=Custom Extrafields for Pages
 Description=Adds custom extrafields to individual pages stored in separate table
-Version=2.7.8
-Date=2026-04-24
+Version=3.0.0
+Date=2026-08-10
 Author=webitproff
-Copyright=Copyright (c) 2026 webitproff
+Copyright=Copyright (c) 2026 webitproff | https://github.com/webitproff
 Notes=BSD License
 Auth_guests=R
 Lock_guests=12345A
 Auth_members=RW
 Lock_members=
 Requires_modules=page
+Recommends_plugins=xtradbrowpage
 [END_COT_EXT]
 ==================== */
 
@@ -22,12 +23,12 @@ defined('COT_CODE') or die('Wrong URL');
 /**
  * xtradbrowpage.setup.php - Register data in $db_core and $db_config. Setup & Config File for the Plugin xtradbrowpage
  *
- * xtradbrowpage plugin for Cotonti 0.9.26, PHP 8.4+
+ * xtradbrowpage plugin for Cotonti v1.+ (https://github.com/Cotonti/Cotonti), PHP 8.5+, MySQL 8.4
  * Filename: xtradbrowpage.setup.php
  *
- * Date: Apr 25Th, 2026
+ * Date: Aug 10Th, 2026
  * @package xtradbrowpage
- * @version 2.7.8
+ * @version 3.0.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowpage-cotonti
  * @license BSD
